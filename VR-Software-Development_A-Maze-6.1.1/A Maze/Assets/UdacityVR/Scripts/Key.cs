970cd0e63b7d9a538e645ee932e4a547058ed378
iamjosephreinoso <69018206+iamjosephreinoso@users.noreply.github.com>
2021-07-20 19:25:00 -0500

    Laberinto VR 6
    Programado comportamiento de la llave

--- VR-Software-Development_A-Maze-6.1.1/A Maze/Assets/UdacityVR/Scripts/Key.cs
+++ VR-Software-Development_A-Maze-6.1.1/A Maze/Assets/UdacityVR/Scripts/Key.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Key : MonoBehaviour {
 
-	// TODO: Create variables to reference the game objects we need access to
-	// Declare a GameObject named 'keyPoofPrefab' and assign the 'KeyPoof' prefab to the field in Unity
-	// Declare a Door named 'door' and assign the top level 'Door' game object to the field in Unity
+    // TODO: Create variables to reference the game objects we need access to
+    // Declare a GameObject named 'keyPoofPrefab' and assign the 'KeyPoof' prefab to the field in Unity
+    // Declare a Door named 'door' and assign the top level 'Door' game object to the field in Unity
+    public GameObject keyPoofPrefab;
 
 
-	void Update () {
-		// OPTIONAL-CHALLENGE: Animate the key rotating
-		// TIP: You could use a method from the Transform class
+    void Update () {
+        // OPTIONAL-CHALLENGE: Animate the key rotating
+        // TIP: You could use a method from the Transform class
+        transform.Rotate(5f, 5f, 5f);
 	}
 
 
@@ -23,8 +25,11 @@ public class Key : MonoBehaviour {
 		/// - Removes the key from the scene
 
 		// Prints to the console when the method is called
+
 		Debug.Log ("'Key.OnKeyClicked()' was called");
 
+        Object.Instantiate(keyPoofPrefab, transform.position, transform.rotation);
+        Destroy(gameObject, 0.5f);
 		// TODO: Unlock the door, display the poof effect, and remove the key from the scene
 		// Use 'door' to call the Door.Unlock() method
 		// Use Instantiate() to create a clone of the 'KeyPoof' prefab at this coin's position and with the 'KeyPoof' prefab's rotation
